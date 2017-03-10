002f0b4e5d3a4c947dcaf5f632c0130b923795c5
De Sande Villarroel <jdesande@ttec.es>
2017-03-10 13:37:31 +0100

    hasta second day

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
 const Koa = require('koa');
 const koaLogger = require('koa-logger');
 const logger = require('logger');
+const body = require('koa-body');
+const mount = require('koa-mount');
+const validate = require('koa-validate');
+const views = require('koa-views');
+const convert = require('koa-convert');
+const session = require('koa-generic-session');
+const File = require('koa-generic-session-file');
+
+const filmRouter = require('routes/film.router');
+const htmlRouter = require('routes/html.router');
+
+
 const app = new Koa();
 if (process.env.NODE_ENV === 'dev') {
     app.use(koaLogger());
 }
+
+app.keys = ['claveSuperSecreta'];
+
+var CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  maxAge: 86400000, /** (number) maxAge in ms (default is 1 days) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+};
+
+
+validate(app);
+
+app.use(body());
+app.use(convert(session(CONFIG, app)));
+
+/*
+app.use(convert(session({
+    store: new File({
+        sessionDirectory: __dirname + '/sessions'
+    })
+})));
+*/
+
 app.use(async (ctx, next) => {
-    const start = Date.now();
+    logger.info(`Last request was ${ctx.session.lastRequest}`);
+    ctx.session.lastRequest = new Date();
     await next();
-    const time = Date.now() - start;
-    //set the header
-    ctx.set('X-Response-Time', `${time} ms`);
 });
-app.use(async (ctx, next) => {
-    logger.debug(`The request url is ${ctx.url}`);
-    ctx.body = {
-        ok: 1
-    };
-});
+
+
+app.use(views(__dirname + '/views', {
+    map: {
+        ejs: 'ejs'
+    }
+}));
+
+
+app.use(htmlRouter.routes());
+app.use(mount('/api/v1', filmRouter.routes()));
+
 app.listen(3000, function (err) {
     if (err) {
         logger.error('Error listening in port 3000', err);
---
+++ routes/html.router.js
@@ -0,0 +1,12 @@
+const logger = require('logger');
+const Router = require('koa-router');
+class HtmlRouter {
+    static async home(ctx) {
+        await ctx.render('index.ejs',{
+                text: ctx.session.lastRequest || 'Empty'
+        });
+    }
+}
+const router = new Router({});
+router.get('/', HtmlRouter.home);
+module.exports = router;
