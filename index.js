f53c4b68374ce442a32a0e60c1f4e01d5cabb9f1
AureGitHub <jdesande@ttec.es>
2017-03-15 09:35:03 +0100

    dsfdsfds

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const inicioRouter = require('routes/inicio.router');
 const gastoRouter = require('routes/gasto.router');
 const tipoGastoRouter = require('routes/tipogasto.router');
 //const mongoUri = 'mongodb://localhost:27017/domestic-accounting';
-const mongoUri = 'mongodb://ds131510.mlab.com:31510/heroku_jhtzpgd7';
+const mongoUri = 'mongodb://aure:jas11jas11@ds131510.mlab.com:31510/heroku_jhtzpgd7';
 
 const error = require('koa-error');
 
