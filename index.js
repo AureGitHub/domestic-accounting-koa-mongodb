367b2754023f641500704a43627c31287af5a6aa
AureGitHub <jdesande@ttec.es>
2017-10-09 16:18:49 +0200

    con cors in server

--- index.js
+++ index.js
@@ -2,14 +2,30 @@
 const Koa = require('koa');
 const body = require('koa-body');
 const authRouter = require('routes/auth.router');
+
+var cors = require('koa-cors');
+
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/domestic-accounting';
+
  const app = new Koa();
 
-  app.use(body());
- app.use(authRouter.routes());
+ const onDBReady = (err) => {
+    if (err) {
+        logger.error('Error connecting', err);
+        throw new Error('Error connecting', err);
+    }
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+    app.use(cors());  
+    app.use(body()); 
+    app.use(authRouter.routes());
+    var port = process.env.PORT || 3000;
+    app.listen(port);
+    console.log('Listening to %s', port);
 
 
 
-console.log('Listening to %s', port);
+}
+
+mongoose.connect(mongoUri, onDBReady);
+ 
